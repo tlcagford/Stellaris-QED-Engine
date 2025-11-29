9b2dbad3c1825ac428f5163ca02c377cccbf2793
Tony E Ford <tlcagford@gmail.com>
2025-11-29 08:59:57 -0700

    Create Source Code Header Template.py

---
+++ Source Code Header Template.py
@@ -0,0 +1,11 @@
+"""
+STELLARIS QED ENGINE - Quantum Vacuum Engineering
+Copyright (c) 2024 Tony Eugene Ford (tlcagford@gmail.com)
+
+This file is part of the STELLARIS QED ENGINE project.
+
+This project is dual-licensed under both the Apache License, Version 2.0 
+and the MIT License. You may choose either license at your option.
+
+See LICENSE-APACHE and LICENSE-MIT for details.
+"""
